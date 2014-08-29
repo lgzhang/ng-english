40f5404c0a71bc069eb7631135ebdc4d050f8160
lgzhang <lgzhang08@gmail.com>
2014-08-29 17:52:26 +0800

    8.29 dong

--- app/js/controllers/listen-exercise.js
+++ app/js/controllers/listen-exercise.js
@@ -1,4 +1,4 @@
-function listenExercise($scope, $http) {
+function listenExercise($scope, $http, $modal) {
 	// 结果页面每个表格最多显示7道题
 	var column = 7;
 
@@ -17,6 +17,25 @@ function listenExercise($scope, $http) {
 		$scope.exerciseListSlice = exerciseListSlice
 		console.log($scope.exerciseListSlice);
 	});
+
+	var myModal = $modal({
+		title: '提示',
+		content: 'My Content',
+		show: false
+	});
+
+	var myOtherModal = $modal({scope: $scope, content: '确定要提交本次作答吗？',title: '提示', placement:'bottom',template: 'modal/docs/modal.tpl.demo.html', confirm:'postAndJump()',backdrop: 'static',show: false});
+
+	// 提交跳转
+	$scope.postAndJump = function() {
+		// $scope.showModal();
+		myOtherModal.$promise.then(myOtherModal.hide);
+		window.location.href = '#/listen-exercise/fail';
+	}
+
+	$scope.showModal = function() {
+		myOtherModal.$promise.then(myOtherModal.show);
+	};
 
 	// 选项
 	$scope.map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
--- app/js/router.js
+++ app/js/router.js
@@ -1,39 +1,49 @@
-angular.module('ng-demo').config(function($stateProvider, $urlRouterProvider, NavData) {
-  $urlRouterProvider.when('', '/home/home');
-  $urlRouterProvider.when('/', '/home/home');
-  $urlRouterProvider.otherwise('/notFound');
+angular.module('ng-demo').config(['$stateProvider',function( $stateProvider) {
+  // $urlRouterProvider.when('', '/home/home');
+  // $urlRouterProvider.when('/', '/home/home');
+  // $urlRouterProvider.when('/listen-exercise', {templateUrl:'view/container/exercise/listen-exercise.html',controller:'listenExercise'});
+  // $urlRouterProvider.when('/listen-exercise/faile',  {templateUrl: 'view/container/exercise/faile.html',controller:'listenExercise'});
+  // $urlRouterProvider.otherwise('/notFound');
   $stateProvider.state('notFound', {
     url: '/notFound',
     controller: 'ctrl.notFound',
     templateUrl: 'views/home/notFound.html'
-  });
+  })
+  .state('listen-exercise',{
+    url:'/listen-exercise',
+    templateUrl:'views/container/exercise/listen-exercise.html'
+  })
+  .state('listen-exercise/fail',{
+    url:'/listen-exercise/fail',
+    templateUrl:'views/container/exercise/fail.html'
+  })
   var states = {};
   // 把多级state弄成单级的，并自动补充父级路由，方便后续处理
-  _.each(NavData, function(group) {
-    _.each(group.items, function(item) {
-      // 处理多级state，自动添加各个父级state
-      var paths = item.state.split(/\./g);
-      var currentPath = '';
-      _.each(paths, function(path) {
-        currentPath += path;
-        states[currentPath] = item;
-        currentPath += '.'
-      });
-    });
-  });
-  // 遵循约定优于配置的原则自动批量注册路由
-  _.each(states, function(item, state) {
-    var path = state.replace(/\./g, '/');
-    var lastState = state.match(/(\w+)$/)[0];
-    $stateProvider.state(state, {
-      url: '/' + lastState,
-      controller: 'ctrl.' + state,
-      templateUrl: 'views/' + path + '.html',
-      label: item.label,
-      files: item.files,
-      description: item.description,
-      authors: item.authors,
-      progress: item.progress
-    });
-  });
-});
+  // _.each(NavData, function(group) {
+  //   _.each(group.items, function(item) {
+  //     // 处理多级state，自动添加各个父级state
+  //     var paths = item.state.split(/\./g);
+  //     var currentPath = '';
+  //     _.each(paths, function(path) {
+  //       currentPath += path;
+  //       states[currentPath] = item;
+  //       currentPath += '.'
+  //     });
+  //   });
+  // });
+  // // 遵循约定优于配置的原则自动批量注册路由
+  // _.each(states, function(item, state) {
+  //   var path = state.replace(/\./g, '/');
+  //   var lastState = state.match(/(\w+)$/)[0];
+  //   $stateProvider.state(state, {
+  //     url: '/' + lastState,
+  //     controller: 'ctrl.' + state,
+  //     templateUrl: 'views/' + path + '.html',
+  //     label: item.label,
+  //     files: item.files,
+  //     description: item.description,
+  //     authors: item.authors,
+  //     progress: item.progress
+  //   });
+  // });
+}]);
