6efdfcbfd5da856e095f4a00557768e425ebf47f
lgzhang <lgzhang08@gmail.com>
2014-08-29 13:57:58 +0800

    8.29

--- app/js/controllers/listen-exercise.js
+++ app/js/controllers/listen-exercise.js
@@ -1,9 +1,25 @@
-function listenExercise($scope,$http) {
-	$http.get('localdata/listen-exercise.json').success(function(data){
+function listenExercise($scope, $http) {
+	// 结果页面每个表格最多显示7道题
+	var column = 7;
+
+	$http.get('localdata/listen-exercise.json').success(function(data) {
 		$scope.exerciseList = data;
+		var exerciseListSlice = [
+			[]
+		];
+		var tempSlice = [];
+		for (var i = 0; i < $scope.exerciseList.length; i++) {
+			exerciseListSlice[exerciseListSlice.length - 1].push(i);
+			if (0 == (i + 1) % column)
+				exerciseListSlice.push([]);
+		}
+		if (exerciseListSlice[exerciseListSlice.length - 1] == 0) exerciseListSlice.pop();
+		$scope.exerciseListSlice = exerciseListSlice
+		console.log($scope.exerciseListSlice);
 	});
 
-	$scope.map = ['A','B','C','D','E','F','G','H','I'];
+	// 选项
+	$scope.map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
 
 	listenExercise.$inject = ['$scope', '$http'];
 }
